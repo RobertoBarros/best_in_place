f3fd1d9df0eaac7d40b14ab4a1482f53c9e9b0f2
Roberto Barros <roberto.f.barros@gmail.com>
2025-04-08 16:23:57 -0300

    test for rails 6.1

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -1,11 +1,11 @@
 require 'rails/railtie'
-require_relative '../best_in_place/helpers'
+require_relative 'helper'
 
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'best_in_place.view_helpers' do
       ActiveSupport.on_load(:action_view) do
-        include BestInPlace::BestInPlaceHelpers
+        include BestInPlace::Helper
       end
     end
   end
